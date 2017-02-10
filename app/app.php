6a0f3cb976c2d56da9ef01dc374f3398559d913b
Dylan <stackhouse.dylan@gmail.com>
2017-02-10 09:29:54 -0800

    add ability to delete all contacts

--- app/app.php
+++ app/app.php
@@ -1,18 +1,39 @@
 <?php
-    require_once __DIR__ . '/../vendor/autoload.php';
-    require_once __DIR__ . '/../src/Contact.php';
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Contact.php";
 
     session_start();
-    if(empty($_SESSION['list_of_contacts']))
-    {
+
+    if (empty($_SESSION['list_of_contacts'])) {
         $_SESSION['list_of_contacts'] = array();
     }
 
     $app = new Silex\Application();
-    $app['debug']= true;
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
-        'twig.path' => __DIR__ . '/../views'
+        'twig.path' => __DIR__.'/../views'
     ));
 
+    $app['debug'] = true;
+
+
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('Contacts.html.twig', array('contacts' =>Contact::getAll()));
+    });
+
+    $app->post('/Create_contact', function() use ($app) {
+        $contact = new Contact($_POST['name'], $_POST['email'], $_POST['phone'], $_POST['address1'], $_POST['address2'] );
+        $contact->save();
+
+        return $app['twig']->render('Create_contact.html.twig', array('contact' => $contact));
+    });
+
+    $app->post('/Delete_contacts', function() use ($app) {
+        Contact::deleteAll();
+        return $app['twig']->render("Delete_contacts.html.twig");
+    });
+
+    return $app;
 ?>
---
+++ src/Contact.php
@@ -0,0 +1,79 @@
+<?php
+    class Contact
+    {
+        private $name;
+        private $email;
+        private $phone;
+        private $address1;
+        private $address2;
+
+        function __construct($name, $email, $phone, $address1, $address2)
+        {
+            $this->name = $name;
+            $this->email = $email;
+            $this->phone = $phone;
+            $this->address1 = $address1;
+            $this->address2 = $address2;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+        function setName($new_name)
+        {
+            $this->name = $new_name;
+        }
+
+        function getEmail()
+        {
+            return $this->email;
+        }
+        function setEmail($new_email)
+        {
+            $this->email = $new_email;
+        }
+
+        function getPhone()
+        {
+            return $this->phone;
+        }
+        function setPhone($new_phone)
+        {
+            $this->phone = $new_phone;
+        }
+
+        function getAddress1()
+        {
+            return $this->address1;
+        }
+        function setAddress1($new_address1)
+        {
+            $this->address1 = $new_address1;
+        }
+
+        function getAddress2()
+        {
+            return $this->address2;
+        }
+        function setAddress2($new_address2)
+        {
+            $this->address2 = $new_address2;
+        }
+
+        function save()
+        {
+            array_push($_SESSION['list_of_contacts'], $this);
+        }
+
+        static function getAll()
+        {
+            return $_SESSION['list_of_contacts'];
+        }
+
+        static function deleteAll()
+        {
+            $_SESSION['list_of_contacts'] = array();
+        }
+    }
+?>
